08e013c24c00ccb3a319603b1d71a6fc3f5f99fd
Josh Junon <git@josh.junon.me>
2025-12-17 19:41:58 +0100

    remove associated trait type for Delay

--- src/delay.rs
+++ src/delay.rs
@@ -1,6 +1,5 @@
-use core::future;
+use core::future::Future;
 
 pub trait Delay {
-    type Future: future::Future<Output = ()>;
-    fn delay_ms(&mut self, ms: u32) -> Self::Future;
+    fn delay_ms(&mut self, ms: u32) -> impl Future<Output = ()>;
 }
